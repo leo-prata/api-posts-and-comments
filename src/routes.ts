582f93d3f749a9471ef35c607ddcc17ec5cb92a8
leo-prata <leo.prata@estudante.ufjf.br>
2025-03-19 11:21:39 -0300

    cria rota para remocao de posts

--- src/routes.ts
+++ src/routes.ts
@@ -2,11 +2,13 @@ import {Router, Request, Response} from 'express';
 
 import CreatePostController from './controllers/post/CreatePostController';
 import ListPostController from './controllers/post/ListPostController';
+import RemovePostController from './controllers/post/RemovePostController';
 
 const router = Router();
 
 // POSTS ROUTES
 router.post('/posts', CreatePostController.handle);
 router.get('/posts', ListPostController.handle);
+router.delete('/posts/:id', RemovePostController.handle);
 
 export {router};
